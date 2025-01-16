ff449e0aa747e7228d5a3d1dde4b7b927ff4fc2f
defson13 <defson.j22@mhs.istts.ac.id>
2025-01-16 19:40:00 +0700

    add Midtrans payment integration and create snap token generation

--- proyek/app/Http/Controllers/TransactionController.php
+++ proyek/app/Http/Controllers/TransactionController.php
@@ -82,6 +82,75 @@ class TransactionController extends Controller
         return $htrans;
     }
 
+    private function getSnapToken($listCart, $htrans, $total)
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        // $params = array(
+        //     'transaction_details' => array(
+        //         'order_id' => rand(),
+        //         'gross_amount' => 10000,
+        //     ),
+        //     'customer_details' => array(
+        //         'first_name' => 'budi',
+        //         'last_name' => 'pratama',
+        //         'email' => 'budi.pra@example.com',
+        //         'phone' => '08111222333',
+        //     ),
+        // );
+
+        // $snapToken = \Midtrans\Snap::getSnapToken($params);
+
+        $products = [];
+        foreach ($listCart as $item) {
+            $products[] = [
+                "id" => $item->Item->ID_items,
+                "price" => $item->Item->price,
+                "quantity" => $item->Item->qty,
+                "name" => $item->Item->name
+            ];
+        }
+
+        $payload = [
+            "transaction_details" => [
+                "order_id" => $htrans->ID_htrans,
+                "gross_amount" => $total
+            ],
+            "customer_details" => [
+                "first_name" => Auth::user()->display_name,
+                "email" => Auth::user()->email,
+                "phone" => Auth::user()->tel,
+                "billing_adress" => [
+                    "address" => Auth::user()->address,
+                ]
+            ],
+            "item_details" => $products
+            // [
+            //     "id" => "a01",
+            //     "price" => 7000,
+            //     "quantity" => 1,
+            //     "name" => "Apple"
+            // ],
+            // [
+            //     "id" => "b02",
+            //     "price" => 3000,
+            //     "quantity" => 2,
+            //     "name" => "Orange"
+            // ]
+
+        ];
+
+        $snapToken = \Midtrans\Snap::getSnapToken($payload);
+
+        return $snapToken;
+    }
+
     public function handleTransaction(Request $req)
     {
         if ($req->has("buy")) {
@@ -97,6 +166,8 @@ class TransactionController extends Controller
             $total = $this->calculateTotalPrice($listCart);
 
             $htrans = $this->createHtrans($listCart, $total);
+
+            $snapToken = $this->getSnapToken($listCart, $htrans, $total);
 
             if (!$htrans) {
                 return redirect()->route('cart')->with('gagal', "Failed to complete the purchase.");
